e07928276bec627ec9e0192ea0a4bc1e539027d2
RIAZxUDDIN <riaz.uddin.p.3000@gmail.com>
2026-03-13 05:24:20 +0600

    learned jagged array

--- Java/Array/jaggedArray.java
+++ Java/Array/jaggedArray.java
@@ -1,4 +1,27 @@
 package Array;
+import java.util.*;
 
 public class jaggedArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[][] arr = new int[3][];
+        arr[0] = new int[3];
+        arr[1] = new int[5];
+        arr[2] = new int[2];
+
+        System.out.print("Enter marks of A,B and C students: ");
+        for(int i=0; i<arr.length; i++) {
+            for(int j=0; j<arr[i].length; j++){
+                arr[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i=0; i<arr.length; i++) {
+            for(int j=0; j<arr[i].length; j++){
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
